ec666415fc9ee984566a70b8363ef6d6d4e0aca8
Jose Elias Romero Guanipa <joseromerogc@gmail.com>
2026-04-29 16:35:02 -0400

    documentacion de algoritmos de ordenamiento

--- courses/rust/basic/estructura-datos/bubble-sort.rs
+++ courses/rust/basic/estructura-datos/bubble-sort.rs
@@ -1,3 +1,28 @@
+//! Algoritmo de ordenamiento Bubble Sort en rust
+//!
+//! Este módulo contiene implementación del algoritmo de ordenamiento Bubble Sort, que es un método
+//! sencillo pero ineficiente para ordenar listas. El algoritmo funciona comparando cada par de
+//! elementos adyacentes y cambiándolos de posición si están en el orden incorrecto. Este proceso
+//! se repite hasta que la lista está ordenada.
+
+/// ordena un slice utilizando el algoritmo de burbuja (Bubble Sort)
+///
+/// Este método es estable y tiene una optimización de "salida temprana"
+/// si la lista se ordena antes de completar todas las interacciones.
+///
+/// # Parámetros
+/// * `lista` - Una referencia mutable a un slice de elementos que implementan `PartialOrd`.
+///
+/// # Ejemplo
+/// ```
+/// let mut numeros = vec![64, 34, 25, 12, 22, 11, 90];
+/// bubble_sort(&mut numeros);
+/// assert_eq!(numeros, vec![11, 12, 22, 25, 34, 64, 90]);
+/// ```
+///
+/// # Complejidad
+/// * Tiempo: O(n^2) en el peor caso, O(n) en el mejor caso (cuando la lista ya está ordenada).
+/// * Espacio: O(1)
 fn bubble_sort<T: PartialOrd>(lista: &mut [T]) {
     let n = lista.len();
 
